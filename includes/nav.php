1f59acb7fad90a35ed739b019f56965dd45502f2
Lasso-Technologies-Ltd <dgichuhi@grouplasso.com>
2016-11-17 14:49:38 +0300

    Create nav.php
    this is the navigation file

---
+++ includes/nav.php
@@ -0,0 +1,40 @@
+ <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+		
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img class = "logo" src="images1/PastedGraphic-1.png" width = "150px" height="100px" alt="The Group Lasso" title="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                     <li><a href="#">Home <span class="glyphicon glyphicon-home"></span></a></li>
+		<li><a href="#">About Us <span class="glyphicon glyphicon-play-circle"></span></a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Subsidiaries <span class="glyphicon glyphicon-road"></span><span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#tech">Lasso Technologies Ltd</a></li>
+            <li><a href="#ener">Lasso Energy Limited</a></li>
+            <li><a href="#coff">Lasso Coffee Ltd</a></li>
+			<li><a href="#elec">Lasso Electronics Ltd</a></li>
+			<li><a href="#inter">Amyra International Ltd</a></li>
+          </ul>
+        </li>
+       
+        <li><a href="#">Contact Us <span class="glyphicon glyphicon-headphones"></span></a></li>
+      </ul>
+      
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
